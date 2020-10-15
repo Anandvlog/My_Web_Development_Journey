86e51b4e5204885498b287d319317cb3c495b0b1
Anandvlog <71122607+Anandvlog@users.noreply.github.com>
2020-10-15 13:55:19 +0530

    Add files via upload

---
+++ Day 9/Js/script.js
@@ -0,0 +1,6 @@
+function onClickMenu() {
+	// body...
+	document.getElementById("menu").classList.toggle("change");
+	document.getElementById("nav").classList.toggle("change");
+	document.getElementById("menu-bg").classList.toggle("change-bg");
+}
